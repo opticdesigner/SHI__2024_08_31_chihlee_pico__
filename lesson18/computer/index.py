fca00040f4a918834201fad8f0d9cb226c093c7b
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-26 15:27:35 +0800

    modify

--- lesson18/computer/index.py
+++ lesson18/computer/index.py
@@ -1,6 +1,24 @@
 import paho.mqtt.client as mqtt
 from datetime import datetime
 import os,csv
+import sqlite3
+from sqlite3 import Error
+
+def insert_to_sqlite(values):
+    try:
+        conn = sqlite3.connect('./data/pico.db')
+    except Exception as e:
+        print(e)
+        return
+    sql = """
+    INSERT INTO 雞舍(時間,設備,值)
+    VALUES(?,?,?)
+    """
+    cursor = conn.cursor()
+    cursor.execute(sql,values)
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 def record(topic:str,value:int | float | str):
     '''
@@ -30,6 +48,7 @@ def record(topic:str,value:int | float | str):
     with open(full_path, mode='a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow([current_str,topic,value])
+        insert_to_sqlite((current_str,topic,float(value))) #加入至資料庫
 
 
 def on_connect(client, userdata, flags, reason_code, properties):
