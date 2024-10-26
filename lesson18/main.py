2aaf5d65e6f0e5ce2e0f9e5bb3db9a37d04fea46
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-26 11:45:01 +0800

    lesson18_1

--- lesson18/main.py
+++ lesson18/main.py
@@ -22,12 +22,12 @@ def do_thing(t):
     conversion_factor = 3.3 / (65535)
     reading = adc.read_u16() * conversion_factor
     temperature = round(27 - (reading - 0.706)/0.001721,2) 
-    #print(f'溫度:{temperature}')
+    print(f'溫度:{temperature}')
     mqtt.publish('SA-01/TEMPERATURE', f'{temperature}')
     adc_value = adc_light.read_u16()
-    #print(f'光線:{adc_value}')
+    print(f'光線:{adc_value}')
     line_state = 0 if adc_value < 3500 else 1
-    #print(f'光線:{line_state}')
+    print(f'光線:{line_state}')
     mqtt.publish('SA-01/LINE_LEVEL', f'{line_state}')
     
     
@@ -40,8 +40,9 @@ def do_thing1(t):
     duty = adc1.read_u16()
     pwm.duty_u16(duty)
     light_level = round(duty/65535*10)
-    #print(f'可變電阻:{light_level}')
+    print(f'可變電阻:{light_level}')
     mqtt.publish('SA-01/LED_LEVEL', f'{light_level}')
+    blynk_mqtt.publish('ds/led_level',f'{light_level}')
     
 
 def main():
@@ -49,7 +50,7 @@ def main():
     print(config.BLYNK_MQTT_BROKER)
     print(config.BLYNK_TEMPLATE_ID)
     print(config.BLYNK_AUTH_TOKEN)
-    blynk_mqtt = MQTTClient(config.BLYNK_TEMPLATE_ID, config.BLYNK_MQTT_BROKER,user='device',password=config.BLYNK_AUTH_TOKEN)
+    blynk_mqtt = MQTTClient(config.BLYNK_TEMPLATE_ID, config.BLYNK_MQTT_BROKER,user='device',password=config.BLYNK_AUTH_TOKEN,keepalive=60)
     blynk_mqtt.connect()
     
         
