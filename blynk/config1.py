fb48a1a0c8d24d7cc255c04195630610057101db
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-26 10:17:09 +0800

    modify

---
+++ blynk/config1.py
@@ -0,0 +1,8 @@
+
+# Fill in information from Blynk Device Info here.
+BLYNK_TEMPLATE_ID   = ""
+BLYNK_TEMPLATE_NAME = "pico"
+BLYNK_AUTH_TOKEN    = "E_FdpF0llQQc0qrii71RZ3WIwx1vngLh"
+
+# Change the default Blynk server. Applicable for users with a white label plan.
+BLYNK_MQTT_BROKER   = "blynk.cloud"
